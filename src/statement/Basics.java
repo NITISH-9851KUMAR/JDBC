ac72860f5e719124a48a8360ee7b3441e58f08aa
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-22 14:04:30 +0530

    Basics of Statement

--- src/statement/Basics.java
+++ src/statement/Basics.java
@@ -1,4 +1,30 @@
 package statement;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import connectionProvider.*;
+
 public class Basics {
+
+    public static void main(String[] args) {
+
+        try{
+
+            Connection connection= getMySQLConnection.getConnection();
+
+            Statement statement= connection.createStatement();
+            String sql= "SELECT * FROM student";
+            ResultSet r= statement.executeQuery(sql);
+
+            if(r.next()){
+                System.out.println(r.getString("name"));
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
