c3a1c58dcf97c0bd6c8a5e4dd6435154e3c46e70
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-22 20:23:57 +0530

    Basics of DB

--- src/statement/Basic.java
+++ src/statement/Basic.java
@@ -1,28 +1,22 @@
 package statement;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.ResultSet;
 
-import connectionProvider.*;
+import connectionProvider.getMySQLConnection;
 
-public class Basics {
-
+public class Basic {
     public static void main(String[] args) {
 
         try{
 
             Connection connection= getMySQLConnection.getConnection();
-
             Statement statement= connection.createStatement();
-            String sql= "SELECT * FROM student";
-            ResultSet r= statement.executeQuery(sql);
 
-            if(r.next()){
-                System.out.println(r.getString("name"));
-            }
+            String sql= "";
 
-        }catch(Exception e){
+        }catch(SQLException e){
             e.printStackTrace();
         }
 
