f0da031073bd52f4c7823fc6976e9b7d8c26bf48
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-11-22 14:00:22 +0530

    Oracle Connection Provider

--- src/connectionProvider/getOracleConnection.java
+++ src/connectionProvider/getOracleConnection.java
@@ -1,4 +1,25 @@
 package connectionProvider;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class getOracleConnection {
+    private static final String url= "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String userName= "system";
+    private static final String password= "nitish";
+
+    Connection connection= null;
+
+    public Connection getConnection(){
+        try{
+            if(connection==null){
+                connection= DriverManager.getConnection(url, userName, password);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
 }
